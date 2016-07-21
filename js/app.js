84f491571e4b003a6c5805138b0920f129afd2c2
sankalp0o <sankalpagarwal294@gmail.com>
2016-07-21 15:29:24 +0530

    Working todo app, now broken into MVC pattern

--- js/app.js
+++ js/app.js
@@ -1,23 +1,58 @@
-var temp;
-
-var deleteItem = function(event){
-	this.parentNode.removeChild(this);
-};
-
-var addToList = function(event){
-	if (event.keyCode === 13) {
-		temp = inp.value;
-		inp.value='';
-		var newItem = document.createElement('div');
-		newItem.addEventListener('click', deleteItem);
-		newItem.innerHTML = temp+"<button>X</button>";
-		cont.appendChild(newItem);
-	}
-};
-
-
 var inp = document.getElementById('todo-input');
-var list = document.getElementById('todo-list');
 var cont = document.getElementById('container-for-list');
-inp.addEventListener('keyup', addToList,  false);
+var list = cont.childNodes;
 
+var initialModel = ['Sample todo item'];
+sessionStorage.setItem('model', JSON.stringify(initialModel));
+
+
+var view = function(model){
+	model.forEach(function(todo, index){
+		var newItem = document.createElement('div');
+		newItem.id = index;
+		newItem.innerHTML = todo+"<button>X</button>";
+		cont.appendChild(newItem);
+	})
+}
+
+
+var controller  = function(storage, view){
+	var model = JSON.parse(storage);
+
+	var addToList = function(event){
+		if (event.keyCode === 13) {
+			var temp = inp.value;
+			model.push(temp);
+			sessionStorage.setItem('model', JSON.stringify(model));	
+			renderView(model);
+			inp.value = "";
+		}
+	};
+
+	var deleteItem = function(event){
+		var indexOfRemoved = this.parentNode.id;
+		model.splice(indexOfRemoved, 1);
+		sessionStorage.setItem('model', JSON.stringify(model));
+		renderView(model);
+	};
+
+	inp.addEventListener('keyup', addToList);
+
+	var renderView = function(model){
+		clearView();
+		view(model);
+		list.forEach(function(todo){todo.firstChild.nextSibling.addEventListener('click', deleteItem)})
+
+	};
+
+	var clearView = function(){
+		while(cont.firstChild){
+			cont.removeChild(cont.firstChild);
+		}
+	}
+
+	renderView(model); 
+
+}
+
+controller(sessionStorage.model, view);
